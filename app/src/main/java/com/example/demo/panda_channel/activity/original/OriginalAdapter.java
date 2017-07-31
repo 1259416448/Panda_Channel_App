08c547da62490a08c901316773625ac38b54e9e3
1259416448 <1259416448@qq.com>
2017-07-31 16:47:49 +0800

    731

--- app/src/main/java/com/example/demo/panda_channel/activity/original/OriginalAdapter.java
+++ app/src/main/java/com/example/demo/panda_channel/activity/original/OriginalAdapter.java
@@ -1,11 +1,14 @@
 package com.example.demo.panda_channel.activity.original;
 
 import android.content.Context;
+import android.content.Intent;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.androidkun.adapter.BaseAdapter;
 import com.androidkun.adapter.ViewHolder;
 import com.example.demo.panda_channel.R;
+import com.example.demo.panda_channel.activity.WebViewActivity;
 import com.example.demo.panda_channel.model.entity.OriginalBean;
 import com.example.demo.panda_channel.net.HttpFactroy;
 
@@ -22,9 +25,19 @@ public class OriginalAdapter extends BaseAdapter<OriginalBean.InteractiveBean>{
     }
 
     @Override
-    public void convert(ViewHolder holder, OriginalBean.InteractiveBean interactiveBean) {
+    public void convert(ViewHolder holder, final OriginalBean.InteractiveBean interactiveBean) {
         final ImageView imageView = (ImageView) holder.itemView.findViewById(R.id.original_list_img);
         HttpFactroy.create().loadImage(interactiveBean.getImage(),imageView);//图片
         holder.setText(R.id.original_list_title,interactiveBean.getTitle());//标题
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context, WebViewActivity.class);
+                intent.putExtra("url",interactiveBean.getUrl());
+                intent.putExtra("title",interactiveBean.getTitle());
+                context.startActivity(intent);
+            }
+        });
     }
 }
